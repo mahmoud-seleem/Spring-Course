caf748c2ac7f06e40997908a4b2ffe0e9e64555b
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-11-12 12:08:04 +0200

    setter injection

--- src/main/java/test/services/FootballCoach.java
+++ src/main/java/test/services/FootballCoach.java
@@ -7,7 +7,10 @@ public class FootballCoach implements Coach {
 
     private Service fortuneService;
 
-    public FootballCoach(Service fortuneService){
+    public FootballCoach(){
+
+    }
+    public void setFortuneService(Service fortuneService){
         this.fortuneService = fortuneService;
     }
     public String callService(){
