554975f0b7ab220979565899a99818f3bda8fca8
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-11-13 13:49:50 +0200

    closing the configuration section of spring course

---
+++ src/main/java/com/example/Spring/Course/test/Config.java
@@ -0,0 +1,9 @@
+package com.example.Spring.Course.test;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(value = "com.example.Spring.Course")
+public class Config {
+}
--- src/main/java/com/example/Spring/Course/test/test.java
+++ src/main/java/com/example/Spring/Course/test/test.java
@@ -1,5 +1,8 @@
 package com.example.Spring.Course.test;
 
+import com.example.Spring.Course.test.services.HiService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.example.Spring.Course.test.interfaces.Coach;
 import com.example.Spring.Course.test.interfaces.Service;
@@ -7,9 +10,11 @@ import com.example.Spring.Course.test.services.FootballCoach;
 
 public class test {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
-        //Service service = context.getBean(Service.class);
+//        ClassPathXmlApplicationContext context =
+//                new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(Config.class);
+//        Service service = context.getBean("myService",Service.class);
         Coach coach = context.getBean("myCoach",Coach.class);
         //System.out.println(service);
         System.out.println(coach.callService());
