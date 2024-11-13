a0038e07d32e37784393e216b224804dae4b7f66
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-11-13 08:41:37 +0200

    spring default behavior without autowiring is calling the no arg constructor

--- src/main/java/com/example/Spring/Course/test/services/HiService.java
+++ src/main/java/com/example/Spring/Course/test/services/HiService.java
@@ -3,7 +3,7 @@ package com.example.Spring.Course.test.services;
 import com.example.Spring.Course.test.interfaces.Service;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-@Primary
+
 @Component
 public class HiService implements Service {
     @Override
